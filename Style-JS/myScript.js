d9215cfa152ac095337d6f66551f20e600b124fa
SpiderGamin <jaytaygame@gmail.com>
2020-01-18 02:41:32 +0000

    Update myScript.js

--- Style-JS/myScript.js
+++ Style-JS/myScript.js
@@ -1,7 +1,13 @@
+//This selects the nav bar button with the "activeonload" id to be clicked
 document.getElementById("activeonload").click();
+
+//When the page is loaded it will set the title to this
 document.title = "Personal Website | Gitlab";
+
+//This is the username variable for a new user
 var userName = "Welcome New User!";
 
+//The navigation bar Script
 function navtwotab(evt, navtwotab) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -16,9 +22,26 @@ function navtwotab(evt, navtwotab) {
     evt.currentTarget.className += " active";
     document.title = (navtwotab);
 }
+
+//When the page loads it tests this to see if you are a new user or not
 if (localStorage.getItem("SpiderGaminName") === null) {
   document.getElementById("getname").innerHTML = userName;
 } else {
     document.getElementById("getname").innerHTML = localStorage.getItem("SpiderGaminName");
 }
 
+//When the settings page loads it will set the text to the name
+function settingsPageLoad() {
+    document.getElementById("namekkk").innerHTML = localStorage.getItem("SpiderGaminName");
+}
+
+//Settings Page: When the name change button is clicked 
+function buttonName() {
+    var nametwo = document.getElementById("input").value;
+    if (nametwo === "") {
+        localStorage.removeItem("SpiderGaminName");
+    } else {
+        localStorage.setItem("SpiderGaminName", "Welcome Back, " + nametwo + "!");
+    }
+    document.getElementById("namekkk").innerHTML = localStorage.getItem("SpiderGaminName");
+}
