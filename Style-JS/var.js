fd45541160709fe94566106247a071ef52004d3d
SpiderGamin <jaytaygame@gmail.com>
2020-02-03 08:54:43 +0000

    Update var.js

--- Style-JS/var.js
+++ Style-JS/var.js
@@ -1,1 +1,10 @@
 // JavaScript Variables
+
+// Varialbles
+var homeContent = "this";
+
+// Set variables to site
+document.getElementById("something").innerHTML = homeContent;
+
+// Testing
+var testvarone = "test yup ok i think it works";
