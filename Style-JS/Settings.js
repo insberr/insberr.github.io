3b5ccfaa857a111e8d9f86fb7858f912e7329247
SpiderGamin <jaytaygame@gmail.com>
2020-02-05 03:39:22 +0000

    Update Settings.js

--- Style-JS/Settings.js
+++ Style-JS/Settings.js
@@ -33,27 +33,20 @@ changeColorInput.addEventListener("keyup", function(event) {
     }
 });
 
-var storedPageColor = loacalStorage.getItem("SpiderGaminPageColor");
-if (storedPageColor === null) {
+if (loacalStorage.getItem("SpiderGaminPageColor") === null) {
     document.getElementsByTagName("body")[0].style.background = "#222222";
 } else {
-    document.getElementsByTagName("body")[0].style.background = storedPageColor;
+    document.getElementsByTagName("body")[0].style.background = loacalStorage.getItem("SpiderGaminPageColor");
 }
 
 function changeColor() {
-    // Set the variable to the inputed color
     var pageColorSet = document.getElementById("changeColorInput").value;
-    // Test if a value was entered
     if (pageColorSet === null) {
-        // If no value then it sets it to the default
-        localStorage.setItem("SpiderGaminPageColor", "#222222");
-    } else if (pageColorSet === " ") {
         localStorage.setItem("SpiderGaminPageColor", "#222222");
     } else {
-        // If there is a value then it sets it to the value entered
         localStorage.setItem("SpiderGaminPageColor", pageColorSet);
-        document.getElementsByTagName("body")[0].style.background = pageColorSet;
     }
+    document.getElementsByTagName("body")[0].style.background = localStorage.getItem("SpiderGaminPageColor");
 }
 
 //
