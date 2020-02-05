7efc6a05a0fe5fc352d22143e60249762809c3cd
SpiderGamin <jaytaygame@gmail.com>
2020-02-05 02:14:08 +0000

    Update Settings.js

--- Style-JS/Settings.js
+++ Style-JS/Settings.js
@@ -1,3 +1,27 @@
 // Site Name
 
 // Page Color
+var storedPageColor = loacalStorage.getItem("SpiderGaminPageColor");
+if (storedPageColor === null) {
+    document.getElementsByTagName("body")[0].style.background = "#222222";
+} else {
+    document.getElementsByTagName("body")[0].style.background = storedPageColor;
+}
+
+function changeColor() {
+    // Set the variable to the inputed color
+    var pageColorSet = document.getElementById("changeColorInput").value;
+    // Test if a value was entered
+    if (pageColorSet === null) {
+        // If no value then it sets it to the default
+        localStorage.setItem("SpiderGaminPageColor", "#222222");
+    } else if (pageColorSet == " ") {
+        localStorage.setItem("SpiderGaminPageColor", "#222222");
+    } else {
+        // If there is a value then it sets it to the value entered
+        localStorage.setItem("SpiderGaminPageColor", pageColorSet);
+        document.getElementsByTagName("body")[0].style.background = pageColorSet;
+    }
+}
+
+//
