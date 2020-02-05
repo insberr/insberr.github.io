6b2fb9554bc381a59fa7e04779f59ce79bdf2f51
SpiderGamin <jaytaygame@gmail.com>
2020-02-05 21:40:53 +0000

    Update List.js

--- Style-JS/List.js
+++ Style-JS/List.js
@@ -1,9 +1,21 @@
-// Current Tab lists
+// Current Tab lists Variables
 // Current
-
+var li1 = "";
+var li2 = "";
+var li3 = "";
+var li4 = "";
+var li5 = "";
 // Completed
 
 //Later Tab lists
 // Personal Later
 
 // Websiste Later 
+
+// Code
+id (li1 === "") {
+    document.getElementsByTagName("li")[0].style
+}
+document.getElementsByTagName("LI")[CI1P].innerHTML = CI1;
+document.getElementsByTagName("li")[CI1P].style.display = CI1D;
+document.getElementsByTagName("li")[CI2P].innerHTML = CI2;
