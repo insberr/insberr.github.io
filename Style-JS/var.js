ad9c1d5294ac7c98eb18cdc20e58a77d86326682
SpiderGamin <jaytaygame@gmail.com>
2020-02-10 03:11:47 +0000

    Update var.js

--- Style-JS/var.js
+++ Style-JS/var.js
@@ -1,3 +1,9 @@
 // JavaScript Variables
-// On load
-document.getElementsByTagName("body")[0].style.background = loacalStorage.getItem("SpiderGaminPageColor");
+
+// On Load (last)
+function onPageLoad() {
+    document.getElementById("outputSecretCode").innerHTML = "Your translated text will go here";
+    document.getElementsByTagName("body")[0].style.background = loacalStorage.getItem("SpiderGaminPageColor");
+}
+
+onPageLoad();
