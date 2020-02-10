cde1fe03de7af8ca28ec23317fcbcb50621817ff
SpiderGamin <jaytaygame@gmail.com>
2020-02-10 03:13:54 +0000

    Update var.js

--- Style-JS/var.js
+++ Style-JS/var.js
@@ -2,8 +2,19 @@
 
 // On Load (last)
 function onPageLoad() {
+    // Secret Code
     document.getElementById("outputSecretCode").innerHTML = "Your translated text will go here";
+    
+    // Page color
     document.getElementsByTagName("body")[0].style.background = loacalStorage.getItem("SpiderGaminPageColor");
+    
+    //When the page loads it tests this to see if you are a new user or not
+    if (localStorage.getItem("SpiderGaminName") === null) {
+        document.getElementById("getname").innerHTML = userName;
+    } else {
+        document.getElementById("getname").innerHTML = localStorage.getItem("SpiderGaminName");
+        document.getElementById("getnameTwo").innerHTML = localStorage.getItem("SpiderGaminName");
+    }
 }
 
 onPageLoad();
