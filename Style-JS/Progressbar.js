f8876e04626ed624af448c6acd2a4b0fe9fc774d
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 21:23:36 +0000

    Fixes and changes to the class selection and variable names

--- Style-JS/Progressbar.js
+++ Style-JS/Progressbar.js
@@ -1,14 +1,14 @@
-var p1 = "50";
-var p1v = 50;
+var pb = "50";
+var pbw = 50;
 
-function progressBar1() {
-    document.getElementById("1progress1").innerHTML = p1;
-    document.getElementById("2progress1").innerHTML = p1;
-    //document.getElementsByClassName("progress1").innerHTML = p1;
-    document.getElementsByClassName("progressbar1").style.width = p1v;
+function progressBarOne() {
+    //document.getElementById("1progress1").innerHTML = p1;
+    //document.getElementById("2progress1").innerHTML = p1;
+    document.getElementsByClassName("progressone").innerHTML = pb;
+    document.getElementsByClassName("progressbarone").style.width = pbw;
 }
 
-progressBar1();
+progressBarOne();
 //set the text in the sentance
 
 //set the progress
