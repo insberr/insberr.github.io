f142316f08857430c1dc7b9f87afac0d137cbb6b
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 16:55:37 +0000

    Fixed something that’s broken

--- Style-JS/Progressbar.js
+++ Style-JS/Progressbar.js
@@ -1,8 +1,8 @@
 var p1 = "23";
 
 function progressBar1() {
-    document.getElementsByClass("progress1").innerHTML = p1;
-    document.getElementsByClass("progressbar1").style.width = p1;
+    document.getElementsByClassName("progress1").innerHTML = p1;
+    document.getElementsByClassName("progressbar1").style.width = p1;
 }
 //set the text in the sentance
 
