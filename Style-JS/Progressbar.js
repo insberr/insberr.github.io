9e4ef63cacf5329fe13f32e2567c581f4e13df36
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 20:02:59 +0000

    Fixes added the function run

--- Style-JS/Progressbar.js
+++ Style-JS/Progressbar.js
@@ -2,10 +2,12 @@ var p1 = "50%";
 var p1v = 50;
 
 function progressBar1() {
-    document.getElementById("1progress1").innerHTML = p1;
-    document.getElementById("2progress1").innerHTML = p1;
-    document.getElementsByClassName("progressbar1").style.width = p1v;
+    document.getElementById("1progress1", "2progress1").innerHTML = p1;
+    //document.getElementById("2progress1").innerHTML = p1;
+    document.getElementsByClassName("progressbar1").style.width = p1;
 }
+
+progressBar1();
 //set the text in the sentance
 
 //set the progress
