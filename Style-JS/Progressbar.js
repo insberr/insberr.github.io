5151a095bbd709ee5e585ea19678059d4c38081e
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 20:09:45 +0000

    Made it so elements can be set by class

--- Style-JS/Progressbar.js
+++ Style-JS/Progressbar.js
@@ -4,6 +4,7 @@ var p1v = 50;
 function progressBar1() {
     document.getElementById("1progress1", "2progress1").innerHTML = p1;
     //document.getElementById("2progress1").innerHTML = p1;
+    //document.getElementsByClassName("progress1").innerHTML = p1;
     document.getElementsByClassName("progressbar1").style.width = p1;
 }
 
