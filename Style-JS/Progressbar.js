569c65ebab1c446b20c2fc3d99b4747e9f9f2428
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 22:03:39 +0000

    Fixed something

--- Style-JS/Progressbar.js
+++ Style-JS/Progressbar.js
@@ -2,6 +2,7 @@ var pb = "50%";
 
 function progressBarOne() {
   document.getElementsByClassName("progressone")[0].innerHTML = pb;
+  document.getElementsByClassName("progressone")[1].innerHTML = pb;
   document.getElementsByClassName("progressbarone")[0].style.width = pb;
 }
 
