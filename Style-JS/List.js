8d30a34d790507e42213fc8b776ef838f3e21d50
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 01:42:04 +0000

    Added the completed list

--- Style-JS/List.js
+++ Style-JS/List.js
@@ -1,8 +1,8 @@
 // --- Current Tab lists Variables
 // Current
-var li1 = "A JavaScript secret code encode/decode thing";
-var li2 = "This websites style"; // !!!DONE!!!
-var li3 = "Using JavaScript to set the content of the list item and the order";
+var li1 = "";
+var li2 = "Using JavaScript to set the content of the list item and the order";
+var li3 = "";
 var li4 = "";
 var li5 = "";
 var li6 = "";
@@ -17,9 +17,21 @@ var li14 = "";
 var li15 = "";
 
 // Completed
-var c1 = "Moving Files";
-var c2 = "Learning HTML and CSS";
-var c3 = "";
+var c1 = "A JavaScript secret code encode/decode thing";
+var c2 = "This websites style";
+var c3 = "Moving Files";
+var c4 = "Learning HTML and CSS";
+var c5 = "";
+var c6 = "";
+var c7 = "";
+var c8 = "";
+var c9 = "";
+var c10 = "";
+var c11 = "";
+var c12 = "";
+var c13 = "";
+var c14 = "";
+var c15 = "";
 
 // --- Later Tab lists
 // Personal Later
@@ -122,8 +134,99 @@ function listsContent() {
 	}
 	
 	// - Completed
+	var completed = document.getElementsByTagName("ul")[1];
+    // List Item 1
+    if (c1 === "") {
+        completed.getElementsByTagName("li")[0].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[0].innerHTML = c1;
+	}
+	// List Item 2
+	if (c2 === "") {
+        completed.getElementsByTagName("li")[1].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[1].innerHTML = c2;
+	}
+	//
+	if (c3 === "") {
+        completed.getElementsByTagName("li")[2].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[2].innerHTML = c3;
+	}
+	//
+	if (c4 === "") {
+        completed.getElementsByTagName("li")[3].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[3].innerHTML = c4;
+	}
+	// 
+	if (c5 === "") {
+        completed.getElementsByTagName("li")[4].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[4].innerHTML = c5;
+	}
+	//
+	if (c6 === "") {
+        completed.getElementsByTagName("li")[5].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[5].innerHTML = c6;
+	}
+	//
+	if (c7 === "") {
+        completed.getElementsByTagName("li")[6].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[6].innerHTML = c7;
+	}
+	//
+	if (c8 === "") {
+        completed.getElementsByTagName("li")[7].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[7].innerHTML = c8;
+	}
+	//
+	if (c9 === "") {
+        completed.getElementsByTagName("li")[8].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[8].innerHTML = c9;
+	}
+	//
+	if (c10 === "") {
+        completed.getElementsByTagName("li")[9].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[9].innerHTML = c10;
+	}
+	//
+	if (c11 === "") {
+        completed.getElementsByTagName("li")[10].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[10].innerHTML = c11;
+	}
+	//
+	if (c12 === "") {
+	    completed.getElementsByTagName("li")[11].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[11].innerHTML = c12;
+	}
+	//
+	if (c13 === "") {
+        completed.getElementsByTagName("li")[12].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[12].innerHTML = c13;
+	}
+	//
+	if (c14 === "") {
+        completed.getElementsByTagName("li")[13].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[13].innerHTML = c14;
+	}
+	//
+	if (c15 === "") {
+        completed.getElementsByTagName("li")[14].style.display = "none";
+    } else {
+        completed.getElementsByTagName("li")[14].innerHTML = c15;
+	}
 	
-	// --- Later
+	// --- To Do Later
 	
 	// - Personal
 	
