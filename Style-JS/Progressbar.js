e20e3bbd75001a6a89d2b3faa801d9bb41d91d08
SpiderGamin <jaytaygame@gmail.com>
2020-02-11 19:18:46 +0000

    Update Progressbar.js

--- Style-JS/Progressbar.js
+++ Style-JS/Progressbar.js
@@ -1,7 +1,7 @@
 var p1 = "23";
 
 function progressBar1() {
-    document.getElementsByClassName("progress1").innerHTML = p1;
+    document.getElementById("1progress1", "2progress1").innerHTML = p1;
     document.getElementsByClassName("progressbar1").style.width = p1;
 }
 //set the text in the sentance
