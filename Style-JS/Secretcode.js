9765d00ac0159df973be56b27efce0dea67e34e7
SpiderGamin <jaytaygame@gmail.com>
2020-06-05 14:36:44 -0700

    Redid the secret code, and added more links

--- Style-JS/Secretcode.js
+++ Style-JS/Secretcode.js
@@ -6,84 +6,187 @@ secretCodeInputEncode.addEventListener("keyup", function(event) {
     }
 });
 
-secretCodeInputDecode.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("codeDecodeButton").click();
-    }
-});
-
 function buttonSecretCodeEncode() {
-    var makeLowerE = document.getElementById("secretCodeInputEncode").value;
-    var secretCodeEncode = makeLowerE.toLowerCase();
-    var code = secretCodeEncode.replace(/a/g, "@");
-    var code = code.replace(/b/g, "/");
-    var code = code.replace(/c/g, "+");
-    var code = code.replace(/d/g, "$");
-    var code = code.replace(/e/g, "3");
-    var code = code.replace(/f/g, "&");
-    var code = code.replace(/g/g, "*");
-    var code = code.replace(/h/g, "(");
-    var code = code.replace(/i/g, "8");
-    var code = code.replace(/j/g, ")");
-    var code = code.replace(/k/g, "'");
-    var code = code.replace(/l/g, "\"");
-    var code = code.replace(/m/g, ":");
-    var code = code.replace(/n/g, ";");
-    var code = code.replace(/o/g, "9");
-    var code = code.replace(/p/g, "0");
-    var code = code.replace(/q/g, "1");
-    var code = code.replace(/r/g, "4");
-    var code = code.replace(/s/g, "#");
-    var code = code.replace(/t/g, "5");
-    var code = code.replace(/u/g, "7");
-    var code = code.replace(/v/g, "=");
-    var code = code.replace(/w/g, "2");
-    var code = code.replace(/x/g, "-");
-    var code = code.replace(/y/g, "6");
-    var code = code.replace(/z/g, "%");
-    var code = code.replace(/\./g, "?");
-    var code = code.replace(/\?/g, ".");
-    var code = code.replace(/!/g, ",");
-    var code = code.replace(/\,/g, "!");
-    var code = code.replace(/ /g, " ");
-    document.getElementById("outputSecretCode").innerHTML = code;
-}
-
-function buttonSecretCodeDecode() {
-    var secretCodeDecode = document.getElementById("secretCodeInputDecode").value;
-    var code = secretCodeDecode.replace(/1/g, "q");
-    var code = code.replace(/2/g, "w");
-    var code = code.replace(/3/g, "e");
-    var code = code.replace(/4/g, "r");
-    var code = code.replace(/5/g, "t");
-    var code = code.replace(/6/g, "y");
-    var code = code.replace(/7/g, "u");
-    var code = code.replace(/8/g, "i");
-    var code = code.replace(/9/g, "o");
-    var code = code.replace(/0/g, "p");
-    var code = code.replace(/@/g, "a");
-    var code = code.replace(/\//g, "b");
-    var code = code.replace(/\+/g, "c");
-    var code = code.replace(/\$/g, "d");
-    var code = code.replace(/&/g, "f");
-    var code = code.replace(/\*/g, "g");
-    var code = code.replace(/\(/g, "h");
-    var code = code.replace(/\)/g, "j");
-    var code = code.replace(/\'/g, "k");
-    var code = code.replace(/\’/g, "k");
-    var code = code.replace(/\”/g, "l");
-    var code = code.replace(/\"/g, "l");
-    var code = code.replace(/\:/g, "m");
-    var code = code.replace(/\;/g, "n");
-    var code = code.replace(/#/g, "s");
-    var code = code.replace(/=/g, "v");
-    var code = code.replace(/-/g, "x");
-    var code = code.replace(/%/g, "z");
-    var code = code.replace(/\./g, "?");
-    var code = code.replace(/\?/g, ".");
-    var code = code.replace(/!/g, ",");
-    var code = code.replace(/\,/g, "!");
-    var code = code.replace(/ /g, " ");
-    document.getElementById("outputSecretCode").innerHTML = code;
+    var codeInput = document.getElementById('secretCodeInput').value.toLowerCase().split('');
+    var output;
+    output = '';
+    codeInput.forEach(character => {
+        switch (character) {
+            case 'a': {
+                return output += '@';
+            }
+            case 'b': {
+                return output += '/';
+            }
+            case 'c': {
+                return output += '+';
+            }
+            case 'd': {
+                return output += '$';
+            }
+            case 'e': {
+                return output += '3';
+            }
+            case 'f': {
+                return output += '&'
+            }
+            case 'g': {
+                return output += '*'
+            }
+            case 'h': {
+                return output += '('
+            }
+            case 'i': {
+                return output += '8'
+            }
+            case 'j': {
+                return output += ')'
+            }
+            case 'k': {
+                return output += '\''
+            }
+            case 'l': {
+                return output += '"'
+            }
+            case 'm': {
+                return output += ':'
+            }
+            case 'n': {
+                return output += ';'
+            }
+            case 'o': {
+                return output += '9'
+            }
+            case 'p': {
+                return output += '0'
+            }
+            case 'q': {
+                return output += '1'
+            }
+            case 'r': {
+                return output += '4'
+            }
+            case 's': {
+                return output += '#'
+            }
+            case 't': {
+                return output += '5'
+            }
+            case 'u': {
+                return output += '7'
+            }
+            case 'v': {
+                return output += '='
+            }
+            case 'w': {
+                return output += '2'
+            }
+            case 'x': {
+                return output += '-'
+            }
+            case 'y': {
+                return output += '6'
+            }
+            case 'z': {
+                return output += '%'
+            }
+            case '1': {
+                return output += 'q';
+            }
+            case '2': {
+                return output += 'w';
+            }
+            case '3': {
+                return output += 'e';
+            }
+            case '4': {
+                return output += 'r';
+            }
+            case '5': {
+                return output += 't';
+            }
+            case '6': {
+                return output += 'y';
+            }
+            case '7': {
+                return output += 'u';
+            }
+            case '8': {
+                return output += 'i';
+            }
+            case '9': {
+                return output += 'o';
+            }
+            case '0': {
+                return output += 'p';
+            }
+            case '.': {
+                return output+= '?';
+            }
+            case '?': {
+                return output += '.';
+            }
+            case '!': {
+                return output += ',';
+            }
+            case ',': {
+                return output += '!';
+            }
+            case '@': {
+                return output += 'a';
+            }
+            case '/': {
+                return output += 'b';
+            }
+            case '+': {
+                return output += 'c';
+            }
+            case '$': {
+                return output += 'd';
+            }
+            case '&': {
+                return output += 'f';
+            }
+            case '*': {
+                return output += 'g';
+            }
+            case '(': {
+                return output += 'h';
+            }
+            case ')': {
+                return output += 'j';
+            }
+            case '\'' || '’': {
+                return output += 'k';
+            }
+            case '"' || '”': {
+                return output += 'l';
+            }
+            case ':': {
+                return output += 'm';
+            }
+            case ';': {
+                return output += 'n';
+            }
+            case '#': {
+                return output += 's';
+            }
+            case '=': {
+                return output += 'v';
+            }
+            case '-': {
+                return output += 'x';
+            }
+            case '%': {
+                return output += 'z';
+            }
+            case ' ': {
+                return output += ' ';
+            }
+            default: {
+                return output += ' '
+            }
+        }
+    });
+    document.getElementById("outputSecretCode").innerHTML = output;
 }
