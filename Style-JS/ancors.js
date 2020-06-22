3bae0db84dd3f9cb365a4ca4e2f3c1a6f85b0c40
unknown <Jeff@JEFF-2011.IDS.lan>
2020-06-22 09:09:10 -0700

    fixes to the hash

--- Style-JS/ancors.js
+++ Style-JS/ancors.js
@@ -7,8 +7,8 @@ var Tab;
 function pageAncors() {
     if (location.hash) {
         var tabHash = location.hash;
-        tabHash.charAt(0).toUpperCase() + tabHash.slice(1);
-        tabHash.replace('#','');
+        tabHash = tabHash.charAt(1).toUpperCase() + tabHash.slice(1);
+        tabHash = tabHash.replace('#','');
         console.log(tabHash);
         removeHash();
         return document.getElementsByClassName(tabHash)[0].click();
