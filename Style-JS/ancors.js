f600f397bd396bbbca1afacbb72f4270dab67adb
unknown <Jeff@JEFF-2011.IDS.lan>
2020-06-22 08:50:11 -0700

    Shortened code

--- Style-JS/ancors.js
+++ Style-JS/ancors.js
@@ -2,47 +2,47 @@ function removeHash() {
     history.pushState("", document.title, window.location.pathname + window.location.search);
 };
 
+var Tab;
+
 function pageAncors() {
     if (location.hash) {
         switch (location.hash) {
             case ('#Home'): {
-                console.log('Home');
-                removeHash()
-                return document.getElementsByClassName('Home')[0].click();
+                Tab = 'Home';
+                break;
             }
             case ('#Tasks'): {
-                console.log('Tasks');
-                removeHash()
-                return document.getElementsByClassName('Tasks')[0].click();
+                Tab = 'Tasks';
+                break;
             }
             case ('#Posts'): {
-                console.log('Posts');
-                removeHash()
-                return document.getElementsByClassName('Posts')[0].click();
+                Tab = 'Posts';
+                break;
             }
             case ('#Code'): {
-                console.log('Code');
-                removeHash()
-                return document.getElementsByClassName('Code')[0].click();
+                Tab = 'Code';
+                break;
             }
             case ('#About'): {
-                console.log('About');
-                removeHash()
-                return document.getElementsByClassName('About')[0].click();
+                Tab = 'About';
+                break;
             }
             case ('#Media'): {
-                console.log('Media');
-                removeHash()
-                return document.getElementsByClassName('Media')[0].click();
+                Tab = 'Media';
+                break;
             }
             case ('#Secret'): {
-                removeHash();
-                return navtwotab(event, 'Secret');
+                navtwotab(event, 'Secret')
+                return;
             }
             default: {
-                return document.getElementsByClassName(local.tab)[0].click();
+                Tab = local.tab;
+                break;
             }
         }
+        console.log(location.hash);
+        removeHash();
+        document.getElementsByClassName(Tab)[0].click();
     } else {
         document.getElementsByClassName(local.tab)[0].click();
     }
