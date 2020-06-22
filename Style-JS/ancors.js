bd1dabd4525c24d23454574e41a7b3fd7bb044c4
unknown <Jeff@JEFF-2011.IDS.lan>
2020-06-21 21:28:13 -0700

    Added # to page for all tabs

--- Style-JS/ancors.js
+++ Style-JS/ancors.js
@@ -5,6 +5,26 @@ function pageAncors() {
                 console.log('Home');
                 return navtwotab(event, 'Home');
             }
+            case ('#Tasks'): {
+                console.log('Tasks');
+                return navtwotab(event, 'Tasks');
+            }
+            case ('#Posts'): {
+                console.log('Posts');
+                return navtwotab(event, 'Posts');
+            }
+            case ('#Code'): {
+                console.log('Code');
+                return navtwotab(event, 'Code');
+            }
+            case ('#About'): {
+                console.log('About');
+                return navtwotab(event, 'About');
+            }
+            case ('#Media'): {
+                console.log('Media');
+                return navtwotab(event, 'Media');
+            }
             case ('#Secret'): {
                 return navtwotab(event, 'Secret');
             }
