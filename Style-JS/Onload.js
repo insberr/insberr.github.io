b5368881324c6d963984c63517b0ea7ca9923d72
unknown <Jeff@JEFF-2011.IDS.lan>
2020-07-03 22:36:58 -0700

    fixes

--- Style-JS/Onload.js
+++ Style-JS/Onload.js
@@ -5,6 +5,16 @@ local = JSON.parse(localStorage.getItem('siteData'));
 console.log(local)
 
 function onPageLoad() {
+    var cookies = confirm("This site uses cookies, click 'ok' to allow cookies, 'cancel' to deny.");
+    if (cookies === true) {
+        return pageLoad(true);
+    } else {
+        localStorage.clear();
+        return pageLoad(false);
+    }
+}
+
+function pageLoad(store) {
     if (local === null) {
         localStorage.clear();
         local = {
@@ -13,22 +23,41 @@ function onPageLoad() {
             name: 'Welcome New User',
             color: {
                 text: 'the default',
-                color: null
+                color: null,
+            cookies: store,
             },
         }
-        pageAncors();
+        if (!local.cookies) {
+            pageAncors();
+    
+            document.title = local.title;
+    
+            document.getElementById("site-user-name").innerHTML = local.name;
+    
+            document.getElementById("outputSecretCode").innerHTML = "Your translated text will go here";
+    
+            // document.getElementsByTagName("body")[0].style.background = local.color.color;
+            document.getElementById("thePageColor").innerHTML = local.color.text;
+        } else {
+            pageAncors();
+    
+            document.title = local.title;
+    
+            document.getElementById("site-user-name").innerHTML = local.name;
+    
+            document.getElementById("outputSecretCode").innerHTML = "Your translated text will go here";
+    
+            // document.getElementsByTagName("body")[0].style.background = local.color.color;
+            document.getElementById("thePageColor").innerHTML = local.color.text;
 
-        document.title = local.title;
-
-        document.getElementById("site-user-name").innerHTML = local.name;
-
-        document.getElementById("outputSecretCode").innerHTML = "Your translated text will go here";
-
-        // document.getElementsByTagName("body")[0].style.background = local.color.color;
-        document.getElementById("thePageColor").innerHTML = local.color.text;
-
-        localStorage.setItem('siteData', JSON.stringify(local));
+            localStorage.setItem('siteData', JSON.stringify(local));
+        }
     } else {
+        var cookies;
+        if (!local.cookies) {
+            cookies = confirm("This site uses cookies, click 'ok' to allow cookies, 'cancel' to deny.");
+        }
+        if (cookies) return;
         console.log(local)
         pageAncors();
 
@@ -43,7 +72,7 @@ function onPageLoad() {
         // localStorage.setItem('siteData', JSON.stringify(local));
     }
 
-    
+
 };
 
 /*
