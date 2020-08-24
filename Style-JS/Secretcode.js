9c5fd41c3f01b5421b3c33f6a220274d939c32ce
Mary <Ipadlover8322@gmail.com>
2020-08-23 23:07:58 -0400

    Spider Gaming
    NjkzNTc0MTQ3NDY0NDk1MTE0.Xn_ItA.rFVO042yhDIfzvRPhYz4geiEUrw

--- Style-JS/Secretcode.js
+++ Style-JS/Secretcode.js
@@ -9,194 +9,70 @@ secretCodeInput.addEventListener("keyup", function(event) {
 
 document.getElementById("outputSecretCode").innerHTML = "Your translated text will go here";
 */
+
+var characterMap = {
+    a: "@",
+    b: "/",
+    c: "+",
+    d: "$",
+    e: "3",
+    f: "&",
+    g: "*",
+    h: "(",
+    i: "8",
+    j: ")",
+    k: "'",
+    l: '"',
+    m: ":",
+    n: ";",
+    o: "9",
+    p: "0",
+    q: "1",
+    r: "4",
+    s: "#",
+    t: "5",
+    u: "7",
+    v: "=",
+    w: "2",
+    x: "-",
+    y: "6",
+    z: "%",
+    "1": "q",
+    "2": "w",
+    "3": "e",
+    "4": "r",
+    "5": "t",
+    "6": "y",
+    "7": "u",
+    "8": "i",
+    "9": "o",
+    "0": "p",
+    ".": "?",
+    "?": ".",
+    "!": ",",
+    ",": "!",
+    "@": "a",
+    "/": "b",
+    "+": "c",
+    $: "d",
+    "&": "f",
+    "*": "g",
+    "(": "h",
+    ")": "j",
+    "'": "k",
+    "‘": "k",
+    '"': "l",
+    "“": "l",
+    ":": "m",
+    ";": "n",
+    "#": "s",
+    "=": "v",
+    "-": "x",
+    "%": "z",
+    " ": " ",
+};
+
 function secretCode(input, callback) {
-    var output; output = '';
-    input.split('').forEach(character => {
-        switch (character.toLowerCase()) {
-            case '\n': {
-                return output += '<br>'
-            }
-            case 'a': {
-                return output += '@';
-            }
-            case 'b': {
-                return output += '/';
-            }
-            case 'c': {
-                return output += '+';
-            }
-            case 'd': {
-                return output += '$';
-            }
-            case 'e': {
-                return output += '3';
-            }
-            case 'f': {
-                return output += '&'
-            }
-            case 'g': {
-                return output += '*'
-            }
-            case 'h': {
-                return output += '('
-            }
-            case 'i': {
-                return output += '8'
-            }
-            case 'j': {
-                return output += ')'
-            }
-            case 'k': {
-                return output += '\''
-            }
-            case 'l': {
-                return output += '"'
-            }
-            case 'm': {
-                return output += ':'
-            }
-            case 'n': {
-                return output += ';'
-            }
-            case 'o': {
-                return output += '9'
-            }
-            case 'p': {
-                return output += '0'
-            }
-            case 'q': {
-                return output += '1'
-            }
-            case 'r': {
-                return output += '4'
-            }
-            case 's': {
-                return output += '#'
-            }
-            case 't': {
-                return output += '5'
-            }
-            case 'u': {
-                return output += '7'
-            }
-            case 'v': {
-                return output += '='
-            }
-            case 'w': {
-                return output += '2'
-            }
-            case 'x': {
-                return output += '-'
-            }
-            case 'y': {
-                return output += '6'
-            }
-            case 'z': {
-                return output += '%'
-            }
-            case '1': {
-                return output += 'q';
-            }
-            case '2': {
-                return output += 'w';
-            }
-            case '3': {
-                return output += 'e';
-            }
-            case '4': {
-                return output += 'r';
-            }
-            case '5': {
-                return output += 't';
-            }
-            case '6': {
-                return output += 'y';
-            }
-            case '7': {
-                return output += 'u';
-            }
-            case '8': {
-                return output += 'i';
-            }
-            case '9': {
-                return output += 'o';
-            }
-            case '0': {
-                return output += 'p';
-            }
-            case '.': {
-                return output+= '?';
-            }
-            case '?': {
-                return output += '.';
-            }
-            case '!': {
-                return output += ',';
-            }
-            case ',': {
-                return output += '!';
-            }
-            case '@': {
-                return output += 'a';
-            }
-            case '/': {
-                return output += 'b';
-            }
-            case '+': {
-                return output += 'c';
-            }
-            case '$': {
-                return output += 'd';
-            }
-            case '&': {
-                return output += 'f';
-            }
-            case '*': {
-                return output += 'g';
-            }
-            case '(': {
-                return output += 'h';
-            }
-            case ')': {
-                return output += 'j';
-            }
-            case '\'': {
-                return output += 'k';
-            }
-            case '‘': {
-                return output += 'k';
-            }
-            case '"': {
-                return output += 'l';
-            }
-            case '“': {
-                return output += 'l';
-            }
-            case ':': {
-                return output += 'm';
-            }
-            case ';': {
-                return output += 'n';
-            }
-            case '#': {
-                return output += 's';
-            }
-            case '=': {
-                return output += 'v';
-            }
-            case '-': {
-                return output += 'x';
-            }
-            case '%': {
-                return output += 'z';
-            }
-            case ' ': {
-                return output += ' ';
-            }
-            default: {
-                return output += ' '
-            }
-        }
-    });
+    var output = input.split('').map(character => characterMap[character]).join('\n');
     callback(output);
 }
