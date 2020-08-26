08fbcec0e3b67227959691078fec6721ba54429e
SpiderGamin <jaytaygame@gmail.com>
2020-08-26 14:01:15 -0700

    Fixed?

--- Style-JS/main.js
+++ Style-JS/main.js
@@ -444,7 +444,7 @@ var posts = new Vue({
             var c = document.getElementsByClassName(`-${postId}`)[0].getElementsByClassName('post-coms')[0];
             let comel = document.querySelectorAll('.post-coms');
             comel.forEach(el => {
-                if (c.style.height = 'fit-content') return;
+                if (c.style.height = 0) return;
                 el.style.height = 0
             });
             if (c) {
